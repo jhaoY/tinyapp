e336a0f07e2d241727f53ef0f88c528b241d8c9f
Justin Hao <justinhao@live.com>
2023-09-04 18:08:12 -0400

    Add route for url/new

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const generateRandomString = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i <= 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello');
@@ -16,6 +27,10 @@ app.get('/', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -30,6 +45,11 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok')
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
