644b854efdbf5a1bad7c3ac1d73cc2ad4aea7da9
Justin Hao <justinhao@live.com>
2023-09-04 18:09:31 -0400

    Fix random string generator

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 const generateRandomString = () => {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i <= 6; i++) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
