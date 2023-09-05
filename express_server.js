776dbfb47c983c87e5ddd95bcea076b1297c0f65
Justin Hao <justinhao@live.com>
2023-09-05 17:45:55 -0400

    Add delete button for index

--- express_server.js
+++ express_server.js
@@ -53,6 +53,12 @@ app.post('/urls', (req, res) => {
   res.redirect(`urls/${id}`)
 })
 
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id]
+  console.log(urlDatabase);
+  res.redirect(`/urls`)
+})
+
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id]
   res.redirect(longURL)
