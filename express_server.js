ecb88e073cb669f8aa496dba003becbf03f8bcd3
Justin Hao <justinhao@live.com>
2023-09-06 18:02:31 -0400

    Moved routes to fix header bug

--- express_server.js
+++ express_server.js
@@ -1,60 +1,93 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
-const { generateRandomString, addUser, findUserByEmail } = require('./helpers.js')
-const { users } = require('./data.js')
+const cookieParser = require('cookie-parser');
+const { generateRandomString, addUser, findUserByEmail } = require('./helpers.js');
+const { users } = require('./data.js');
 
 const app = express();
 const PORT = 8080;
 
-app.use(cookieParser())
-app.use(express.static('public'))
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+// Middleware for user authentication
+app.use((req, res, next) => {
+  const userId = req.cookies.user_id;
+  res.locals.user = users[userId]; // Make user available to all templates
+  next();
+});
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-
+// Home page
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+// Register routes
+app.get('/register', (req, res) => {
+  res.render('urls_register');
+});
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  if (email === '' || password === '') {
+    res.status(400);
+    res.send('Invalid email and/or password');
+  } else if (findUserByEmail(email)) {
+    res.status(400);
+    res.send('User already exists');
+  } else {
+    const userID = addUser(email, password);
+    res.cookie('user_id', userID);
+    console.log(users);
+    res.redirect('/urls');
+  }
+});
+
+// Login routes
+app.get('/login', (req, res) => {
+  res.render('urls_login');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const user = findUserByEmail(email);
+  if (!user) {
+    res.status(403);
+    res.send('Email does not exist');
+  } else if (user.password !== password) {
+    res.status(403);
+    res.send('Incorrect password');
+  } else {
+    const userID = user.id;
+    res.cookie('user_id', userID);
+    res.redirect('/urls');
+  }
+});
+
+// Logout route
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+});
+
+// URL related routes
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    user: users[req.cookies['user_id']]
+    user: users[req.cookies.user_id],
   };
   res.render('urls_index', templateVars);
 });
 
-app.get('/register', (req, res) => {
-  res.render('urls_register')
-})
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
-app.post('/register', (req, res) => {
-  const { email, password } = req.body;
-  if (email === "" || password === "") {
-    res.status(400);
-    res.send('Invalid email and/or password')
-  } else if (findUserByEmail(email)) {
-    res.status(400);
-    res.send('User already exists')
-  }
-  const userID = addUser(email, password);
-  res.cookie('user_id', userID)
-  console.log(users);
-  res.redirect('/urls')
-})
-
-app.get('/login', (req, res) => {
-  res.render('urls_login')
-})
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -62,64 +95,35 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:id/update', (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.newLongURL;
+  res.redirect('/urls');
+});
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  console.log(urlDatabase);
+  res.redirect('/urls');
+});
+
+// Redirect short URLs to their corresponding long URLs
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+// JSON endpoint for URL database
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post('/urls', (req, res) => {
-  console.log(req.body);
-  let id = generateRandomString();
-  urlDatabase[id] = req.body.longURL
-  res.redirect(`urls/${id}`)
-})
-
-app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id]
-  console.log(urlDatabase);
-  res.redirect(`/urls`)
-})
-
-app.post('/urls/:id', (req, res) => {
-  res.redirect(`/urls/${req.params.id}`)
-})
-
-app.post('/urls/:id/update', (req, res) => {
-  const id = req.params.id;
-  urlDatabase[id] = req.body.newLongURL
-  res.redirect('/urls')
-})
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  const user = findUserByEmail(email);
-  if (!user) {
-    res.status(403)
-    res.send("Email does not exist")
-  } else if (user.password !== password) {
-    res.status(403)
-    res.send("Incorrect password")
-  } else {
-    const userID = user.id
-    res.cookie('user_id', userID)
-    res.redirect('/urls');
-  }
-})
-
-app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-})
-
-app.get('/u/:id', (req, res) => {
-  const longURL = urlDatabase[req.params.id]
-  res.redirect(longURL)
-})
+// URL database
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
