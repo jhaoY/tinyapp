fd1844360dd91afe2b5c3c87db6e48250a49622e
Justin Hao <justinhao@live.com>
2023-09-06 15:05:59 -0400

    Move functions to helpers.js

---
+++ helpers.js
@@ -0,0 +1,13 @@
+// Function to generate a random string for shortURL ID
+const generateRandomString = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+module.exports = {
+  generateRandomString
+}
